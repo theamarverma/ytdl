9d406e8a0da102cd7f2e7436399815baf81f888c
Tarun Verma <tarun.verma@wheebox.com>
2022-11-22 18:01:45 +0530

    added info

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 const url = 'https://www.youtube.com/watch?v=JmOzco_KT3g';
-ytdl(url).pipe(fs.createWriteStream('video.mp4'));
+//ytdl(url).pipe(fs.createWriteStream('video.mp4'));
+
+console.log(`url =${url}`);
